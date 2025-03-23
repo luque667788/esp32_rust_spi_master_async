16e211526a030c97d30fdd2920b33518a1e494c2
Luiz Mendonca <luquemendonca@gmail.com>
2025-03-23 13:26:26 +0100

    weird cs interrupt bug or something

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -11,7 +11,7 @@
 //! 12 -> 22
 //! 11 -> 35
 //! 10 -> 27
-//! 
+//!
 //! The following wiring is assumed for the (bitbang) master:
 //! - SCLK => GPIO27
 //! - MISO => GPIO35
@@ -38,12 +38,14 @@
 use embassy_executor::Spawner;
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, watch::Watch};
 use embassy_time::{Duration, Timer};
+use embedded_hal::digital::OutputPin;
 use esp_backtrace as _;
 use esp_hal::{
     gpio::{Event, Io},
     handler,
     peripheral::Peripheral,
     ram,
+    spi::master::Spi,
     timer::timg::TimerGroup,
 };
 
@@ -52,16 +54,12 @@ use esp_hal::{
     delay::Delay,
     dma_buffers,
     gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull},
-    main,
-    spi,
+    main, spi,
 };
 use esp_println::println;
 
 static mut MASTER_RECEIVE: [u8; 32000] = [0; 32000];
 static mut MASTER_SEND: [u8; 32000] = [0; 32000];
-
-
-
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
@@ -72,29 +70,40 @@ async fn main(spawner: Spawner) {
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_hal_embassy::init(timg0.timer0);
+    /*
+        let mut master_sclk = Output::new(peripherals.GPIO27, Level::Low, OutputConfig::default());
+        let master_miso = Input::new(
+            peripherals.GPIO35,
+            InputConfig::default().with_pull(Pull::None),
+        );
+        let mut master_mosi = Output::new(peripherals.GPIO22, Level::Low, OutputConfig::default());
 
-
-
-    let mut master_sclk = Output::new(peripherals.GPIO27, Level::Low, OutputConfig::default());
-    let master_miso = Input::new(
-        peripherals.GPIO35,
-        InputConfig::default().with_pull(Pull::None),
-    );
-    let mut master_mosi = Output::new(peripherals.GPIO22, Level::Low, OutputConfig::default());
+        */
     let mut master_cs = Output::new(peripherals.GPIO21, Level::High, OutputConfig::default());
-
-
+    
     let mut master_send = unsafe { &mut MASTER_SEND };
     let mut master_receive = unsafe { &mut MASTER_RECEIVE };
-    
+
     let mut i = 0;
 
     // Initialize master_send with a simple pattern
     master_send.fill(0xAA); // Fill with pattern 10101010
 
     // Set up the CS interrupt
-    spawner.spawn(get_slave_dma_buffer()).unwrap();
+    spawner.spawn(background_task()).unwrap();
 
+    let mut master_spi = Spi::new(
+        peripherals.SPI3,
+        esp_hal::spi::master::Config::default()
+            .with_frequency(esp_hal::time::Rate::from_khz(100))
+            .with_mode(esp_hal::spi::Mode::_0),
+    )
+    .unwrap()
+    .with_sck(peripherals.GPIO27)
+    .with_mosi(peripherals.GPIO22)
+    .with_miso(peripherals.GPIO35)
+    //.with_cs(master_cs)
+    .into_async();
 
     loop {
         // Update a few key positions to track iterations
@@ -104,84 +113,41 @@ async fn main(spawner: Spawner) {
         master_send[master_send.len() - 1] = i; // Last byte also shows iteration count
 
         i = i.wrapping_add(1); // Increment counter, wrapping at 255
-        
 
-        bitbang_master(
-            master_send,
-            master_receive,
-            &mut master_cs,
-            &mut master_mosi,
-            &mut master_sclk,
-            &master_miso,
-        )
-        .await;
-    println!(
-        "sent stuff {:x?} .. {:x?}",
-        &master_send[..10],
-        &master_send[master_send.len() - 10..],
-    );
+        //write_data_spi_async(&mut master_spi, master_send, &mut master_cs).await.unwrap();
+        // Write data to SPI
+        master_cs.set_low();
+        master_spi.write(master_send).unwrap();
+        master_cs.set_high();
+        println!(
+            "sent stuff {:x?} .. {:x?}",
+            &master_send[..10],
+            &master_send[master_send.len() - 10..],
+        );
         Timer::after(Duration::from_millis(5000)).await;
-
-        
     }
 }
 
-async fn bitbang_master(
-    master_send: &[u8],
-    master_receive: &mut [u8],
-    master_cs: &mut Output<'_>,
-    master_mosi: &mut Output<'_>,
-    master_sclk: &mut Output<'_>,
-    master_miso: &Input<'_>,
-) {
-    // Bit-bang out the contents of master_send and read into master_receive
-    // as quickly as manageable. MSB first. Mode 0, so sampled on the rising
-    // edge and set on the falling edge.
-    println!("bitbang_master writing to spi");
-    master_cs.set_low();
-    for (j, v) in master_send.iter().enumerate() {
-        let mut b = *v;
-        let mut rb = 0u8;
-        for _ in 0..8 {
-            if b & 128 != 0 {
-                master_mosi.set_high();
-            } else {
-                master_mosi.set_low();
-            }
-            master_sclk.set_low();
-            b <<= 1;
-            rb <<= 1;
-            // NB: adding about 24 NOPs here makes the clock's duty cycle
-            // run at about 50% ... but we don't strictly need the delay,
-            // either.
-            master_sclk.set_high();
-            if master_miso.is_high() {
-                rb |= 1;
-            }
-        }
-        master_receive[j] = rb;
-    }
-    master_sclk.set_low();
-
-    // Add a small delay before raising CS to ensure data transmission is complete
-
-    master_cs.set_high();
-    master_sclk.set_low();
+async fn write_data_spi_async(
+    spi: &mut Spi<'_, esp_hal::Async>,
+    data: &[u8], cs: &mut Output<'_>,
+) -> Result<(), spi::Error> {
+    // Write data to SPI
+    println!("cs is low now");
+    cs.set_low(); // Set CS low to start transmission
+    embedded_hal_async::spi::SpiBus::write(spi, data).await?;
+    cs.set_high(); // Set CS high to end transmission
+    Ok(()) // Return Ok to satisfy the Result return type
 }
 
 // This task runs in the background and can be used to perform other operations
 #[embassy_executor::task]
-async fn get_slave_dma_buffer() {
-
+async fn background_task() {
     loop {
         // Wait for a short duration between checks
         Timer::after(Duration::from_millis(1000)).await;
 
         // Print a message to show the task is running
         println!("Just some background task...");
-
-        
     }
 }
-
-
